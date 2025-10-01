ad6db8e458972228e88f77382c07fe6af085f2cf
Martin Emch <martin.emch@contractors.roche.com>
2025-10-01 16:26:29 +0200

    formatting

--- Arduino/MouseEmulator/MouseEmulator01/MouseEmulator01.ino
+++ Arduino/MouseEmulator/MouseEmulator01/MouseEmulator01.ino
@@ -1,34 +1,20 @@
 /* -----------------------------------------------------------------------------
   (C) Copyright by the respective owner
   ------------------------------------------------------------------------------
-  Controls the mouse from five pushbuttons on an Arduino Leonardo, Micro or Due.
-
-  Hardware:
-  - five pushbuttons attached to D2, D3, D4, D5, D6
-
-  The mouse movement is always relative. This sketch reads four pushbuttons, and
-  uses them to set the movement of the mouse.
-
+  Controls the mouse from an Arduino Leonardo, Micro or Due.
+  The mouse movement is always relative. 
   WARNING: When you use the Mouse.move() command, the Arduino takes over your
   mouse! Make sure you have control before you use the mouse commands.
-
-  created 15 Mar 2012
-  modified 27 Mar 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
   https://docs.arduino.cc/built-in-examples/usb/KeyboardAndMouseControl/
   --------------------------------------------------------------------------- */
 #if not defined(ARDUINO_AVR_LEONARDO)
-  #error "only for ARDUINO_AVR_LEONARDO"
+#error "only for ARDUINO_AVR_LEONARDO"
 #endif
 // -----------------------------------------------------------------------------
 #include "Mouse.h"
 // -----------------------------------------------------------------------------
 #define MAX_COUNT 3
 uint16_t uCount = 0;
-// -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 void setup() {
   Mouse.begin();
