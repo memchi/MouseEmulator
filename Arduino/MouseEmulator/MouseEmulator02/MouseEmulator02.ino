b6d198469a790ac5fe2fa9e83ea0bd035eba0800
Martin Emch <martin.emch@contractors.roche.com>
2025-10-06 10:45:37 +0200

    measured coordinates correct, seems to fast

--- Arduino/MouseEmulator/MouseEmulator02/MouseEmulator02.ino
+++ Arduino/MouseEmulator/MouseEmulator02/MouseEmulator02.ino
@@ -15,16 +15,18 @@
 // -----------------------------------------------------------------------------
 #define SWITCH_INPUT 4
 // -----------------------------------------------------------------------------
-#define MOVE_UPPER_LEFT_X -3840
-#define MOVE_UPPER_LEFT_Y -2160
-#define CLICK_01_X_ABS 300
-#define CLICK_01_Y_ABS 300
-#define CLICK_02_X_REL 200
-#define CLICK_02_Y_REL 0
-#define CLICK_03_X_REL -200
-#define CLICK_03_Y_REL 100
-#define CLICK_04_X_REL 200
-#define CLICK_04_Y_REL 100
+#define MOVE_UPPER_LEFT_X -640
+#define MOVE_UPPER_LEFT_Y -480
+#define CLICK_OVERVIEW_X_ABS 100
+#define CLICK_OVERVIEW_Y_ABS 60
+#define CLICK_UTILITIES_X_REL 335
+#define CLICK_UTILITIES_Y_REL 0
+#define CLICK_CONFIGURATION_X_REL -295
+#define CLICK_CONFIGURATION_Y_REL 155
+#define CLICK_CLOSE_X_REL 335
+#define CLICK_CLOSE_Y_REL 220
+// -----------------------------------------------------------------------------
+bool bFirstRound = true;
 // -----------------------------------------------------------------------------
 void MouseMove16(int_fast16_t x, int_fast16_t y) {
   int_fast16_t x_moved = 0;
@@ -54,6 +56,7 @@ void MouseMove16(int_fast16_t x, int_fast16_t y) {
     } else {
       // no more y move
     }
+    //delay(5);
   }
   digitalWrite(LED_BUILTIN, LOW);
 }
@@ -63,26 +66,30 @@ void setup() {
   pinMode(SWITCH_INPUT, INPUT_PULLUP);
   Mouse.begin();
   delay(1000);
-  MouseMove16(MOVE_UPPER_LEFT_X, MOVE_UPPER_LEFT_Y);  // move upper left
-  delay(1000);
-  MouseMove16(CLICK_01_X_ABS, CLICK_01_Y_ABS);  // move to start
-  delay(1000);
+  bFirstRound = true;
 }
 // -----------------------------------------------------------------------------
 void loop() {
   if (!digitalRead(SWITCH_INPUT)) {
-    Mouse.click(MOUSE_LEFT);
-    MouseMove16(CLICK_02_X_REL, CLICK_02_Y_REL);
+    if (bFirstRound) {
+      MouseMove16(MOVE_UPPER_LEFT_X, MOVE_UPPER_LEFT_Y);        // move upper left
+      MouseMove16(CLICK_OVERVIEW_X_ABS, CLICK_OVERVIEW_Y_ABS);  // move to overview
+      bFirstRound = false;
+    }
     delay(1000);
     Mouse.click(MOUSE_LEFT);
-    MouseMove16(CLICK_03_X_REL, CLICK_03_Y_REL);
+    MouseMove16(CLICK_UTILITIES_X_REL, CLICK_UTILITIES_Y_REL);  // move to utilties
     delay(1000);
     Mouse.click(MOUSE_LEFT);
-    MouseMove16(CLICK_04_X_REL, CLICK_04_Y_REL);
+    MouseMove16(CLICK_CONFIGURATION_X_REL, CLICK_CONFIGURATION_Y_REL);  // move to configuration
     delay(1000);
     Mouse.click(MOUSE_LEFT);
-    MouseMove16(-CLICK_02_X_REL - CLICK_03_X_REL - CLICK_04_X_REL, -CLICK_02_Y_REL - CLICK_03_Y_REL - CLICK_04_Y_REL);
+    MouseMove16(CLICK_CLOSE_X_REL, CLICK_CLOSE_Y_REL);  // move to close
     delay(1000);
+    Mouse.click(MOUSE_LEFT);
+    MouseMove16(-CLICK_UTILITIES_X_REL - CLICK_CONFIGURATION_X_REL - CLICK_CLOSE_X_REL, -CLICK_UTILITIES_Y_REL - CLICK_CONFIGURATION_Y_REL - CLICK_CLOSE_Y_REL);  // move back to overview
+  } else {
+    bFirstRound = true;
   }
 }
 // -----------------------------------------------------------------------------
